fad36cf8989aefcb673f18185898e2e161acaf2e
Bryan Lemes <bryansilvalemes@gmail.com>
2024-01-08 23:44:51 -0300

    Adicionada barra lateral cadastro de pessoa

---
+++ cadastro/cadastro.js
@@ -0,0 +1,17 @@
+/*
+function handleResize() {
+    //var alturaDaDiv = document.querySelector("#lateral").clientHeight;
+
+    if(window.clientWidth >= 1451){
+        document.querySelector("#lateral").clientHeight = 1000;
+        console.log('tudo certo2');
+    }
+
+    console.log('tudo certo1');
+    //window.clientHeight;
+}
+
+
+window.addEventListener('resize', handleResize);
+
+*/
