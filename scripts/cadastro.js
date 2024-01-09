206f247d7b5e37f6ba1126c0729d20d93fffaaac
BryanMzili <bryansilvalemes@gmail.com>
2024-01-09 11:40:33 -0300

    Início Tela Login

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -12,3 +12,21 @@ function handleResize() {
 }
 
 window.addEventListener('resize', handleResize);
+
+function cadastrar(){
+    var nome = document.querySelector("#nome");
+    var cpf = document.querySelector("#cpf");
+    var nascimento = document.querySelector("#nascimento");
+    var endereco = document.querySelector("#endereco");
+    var email = document.querySelector("#email");
+    var telefone = document.querySelector("#telefone");
+}
+
+function limpar(){
+    document.querySelector("#nome").value = "";
+    document.querySelector("#cpf").value = "";
+    document.querySelector("#nascimento").value = "";
+    document.querySelector("#endereco").value = "";
+    document.querySelector("#email").value = "";
+    document.querySelector("#telefone").value = "";
+}
